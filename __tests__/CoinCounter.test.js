c4eed5c1c38f54bd5ef5cf5810d2a8d9928e4912
me <me@me.me.com>
2021-02-12 15:35:35 -0800

    implement currying and closures

--- __tests__/CoinCounter.test.js
+++ __tests__/CoinCounter.test.js
@@ -8,4 +8,14 @@ describe('CoinCounter', () => {
     expect(builder["dime"]).toEqual(1);
     expect(builder["nickel"]).toEqual(1);
     expect(builder["penny"]).toEqual(4);
-})});
+  })
+
+  test('should return correct values on closed functions', () => {
+    const coinCounter = new CoinCounter();
+    let builder = coinCounter.OuterFunction(169);
+    expect(builder("quarter")).toEqual(6);
+    expect(builder("dime")).toEqual(16);
+    expect(builder("nickel")).toEqual(33);
+    expect(builder("penny")).toEqual(169);
+  })
+});
--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,25 +1,51 @@
 class CoinCounter {
 
-  recursiveCounter(number, builder = {}) {
-    if (number >= 25) {
-      builder["quarter"] = (builder["quarter"] || 0) + 1;
-      return this.recursiveCounter(number - 25, builder);
-    }
-    else if (number >= 10) {
-      builder["dime"] = (builder["dime"] || 0) + 1;
-      return this.recursiveCounter(number - 10, builder);
-    }
-    else if (number >= 5) {
-      builder["nickel"] = (builder["nickel"] || 0) + 1;
-      return this.recursiveCounter(number - 5, builder);
-    }
-    else if (number >= 1) {
-      builder["penny"] = (builder["penny"] || 0) + 1;
-      return this.recursiveCounter(number - 1, builder);
-    }
-    else if (number <= 0) {
-      return builder;
-    }
+  recursiveCounter(amount, builder = {}) {
+    let coinObject =
+    {
+      25: "quarter",
+      5: "nickel",
+      10: "dime",
+      1: "penny"
+    };
+    let keys = [1, 5, 10, 25];//ordered list of coin values
+    //get highest coin
+    const reducer = function (accumulator, currentValue) {
+      if (amount >= currentValue) {
+        return currentValue;
+      }
+      else {
+        return accumulator;
+      }
+    };
+
+    let value = keys.reduce(reducer, 0);
+
+    if (value == 0) return builder;
+    let coin = coinObject[value];
+
+    builder[coin] = (builder[coin] || 0) + 1;
+
+    return this.recursiveCounter(amount - value, builder);
+  }
+
+  OuterFunction(amount) {
+    return function (change) {
+      return (function recursiveCounter2(amount) {
+        let coinObject =
+        {
+          "quarter": 25,
+          "nickel": 5,
+          "dime": 10,
+          "penny": 1
+        };
+        let value = coinObject[change];
+        if (amount >= value) {
+          return 1 + recursiveCounter2(amount - value);
+        }
+        return 0;
+      })(amount);
+    };
   }
 }
 export default CoinCounter;
